71593751dda52da46165134bd7ea334004895228
Orhan Albayati <70097003+Warchiefen@users.noreply.github.com>
2021-02-18 13:23:17 +0100

    Installed Ajax, Tying to write reg.field into JSON

--- script.js
+++ script.js
@@ -18,3 +18,31 @@ function renderButton() {
   });
 }
 //GOOGLE SCRIPT LOGIN END************************
+
+/*ADD REGISTER FIELD INTO A JSON FILE*/
+// $(document).ready(function () {
+//   $(".btn").click(function (e) {
+//     var jsonData = {};
+
+//     var formData = $("#myform").serializeArray();
+//     // console.log(formData);
+
+//     $.each(formData, function () {
+//       if (jsonData[this.name]) {
+//         if (!jsonData[this.name].push) {
+//           jsonData[this.name] = [jsonData[this.name]];
+//         }
+//         jsonData[this.name].push(this.value || "");
+//       } else {
+//         jsonData[this.name] = this.value || "";
+//       }
+//     });
+//     console.log(jsonData);
+//     $.ajax({
+//       url: "action.php",
+//       type: "POST",
+//       data: jsonData,
+//     });
+//     e.preventDefault();
+//   });
+// });
