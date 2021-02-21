383f23f4b7e579c16ecd9ce6ebecffce242dd8e7
Orhan Albayati <70097003+Warchiefen@users.noreply.github.com>
2021-02-21 14:16:37 +0100

    Removed out-commented google login script

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 "use strict";
-// GOOGLE SCRIPT LOGIN START**********************
-// function onSuccess(googleUser) {
-//   console.log("Logged in as: " + googleUser.getBasicProfile().getName());
-// }
-// function onFailure(error) {
-//   console.log(error);
-// }
-// function renderButton() {
-//   gapi.signin2.render("my-signin2", {
-//     scope: "profile email",
-//     width: 240,
-//     height: 50,
-//     longtitle: true,
-//     theme: "dark",
-//     onsuccess: onSuccess,
-//     onfailure: onFailure,
-//   });
-// }
-//GOOGLE SCRIPT LOGIN END************************
 
-//REGISTER
-//Skapa en array som tar emot object "member".  (Gör den global i skriptet)
-//member objectet har 3 properties. username, email, password.
-//Om samma email eller samma username redan existerar så pushar vi inte in inputen till member objectet.
-
-// registerClick.addEventListener("click", function () {
-//   //FUNCTION HERE
-// });
-
-//LOGIN
+//LOGIN SECTION **************************************************
 //Starting members
 const account1 = {
   username: "warchief",
@@ -50,11 +22,6 @@ const account4 = {
   password: "villhajasmine",
 };
 
-//Force username inputs to be lowercase on registration AND login
-function forceLower(strInput) {
-  strInput.value = strInput.value.toLowerCase();
-}
-
 let accounts = [account1, account2, account3, account4];
 
 const loginButton = document.querySelector(".login-btn");
@@ -71,7 +38,7 @@ loginButton.addEventListener("click", function (e) {
   currentAccount = accounts.find(
     (acc) => acc.username === inputLoginUser.value
   );
-  //Optional chaining
+  //Optional chaining (?.)
   if (currentAccount?.password === inputLoginPsw.value) {
     console.log(currentAccount);
     labelWelcome.textContent = `Welcome back, ${currentAccount.username}`;
@@ -81,10 +48,10 @@ loginButton.addEventListener("click", function (e) {
     alert("Wrong Username or Password.");
   }
 });
+//*****************************************************************************/
 
-//REGISTER A USER
+//REGISTER A USER*******************************
 const registerButton = document.querySelector(".btn");
-
 const inputRegUser = document.querySelector(".input-field-user");
 const inputRegPsw = document.querySelector(".input-field-psw");
 
@@ -107,3 +74,10 @@ registerButton.addEventListener("click", function (event) {
   }
 });
 console.log(accounts);
+//************************************************************************************************* */
+
+//Force username inputs to be lowercase on registration AND login
+function forceLower(strInput) {
+  strInput.value = strInput.value.toLowerCase();
+}
+//******************************************************************** */
