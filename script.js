0e5f363383057fbd661086f52392d71b964cf211
Orhan Albayati <70097003+Warchiefen@users.noreply.github.com>
2021-02-23 12:22:44 +0100

    Removed old login/reg section added OKTA signin

--- script.js
+++ script.js
@@ -1,88 +1,43 @@
 "use strict";
 
-//LOGIN SECTION **************************************************
-//Starting members
-const account1 = {
-  username: "warchief",
-  password: "hejsan",
-};
-
-const account2 = {
-  username: "orhan",
-  password: "svejsan",
-};
-
-const account3 = {
-  username: "mofasa",
-  password: "truckförare",
-};
-
-const account4 = {
-  username: "jafar",
-  password: "villhajasmine",
-};
-
-let accounts = [account1, account2, account3, account4];
-
-const loginButton = document.querySelector(".login-btn");
-
-const inputLoginUser = document.querySelector(".login__input--user");
-const inputLoginPsw = document.querySelector(".login__input--psw");
-
-const labelWelcome = document.querySelector(".welcome");
-
-let currentAccount;
-
-//LOGIN FUNCTION
-loginButton.addEventListener("click", function (e) {
-  currentAccount = accounts.find(
-    (acc) => acc.username === inputLoginUser.value
-  );
-  //Optional chaining (?.)
-  if (currentAccount?.password === inputLoginPsw.value) {
-    console.log(currentAccount);
-    labelWelcome.textContent = `Welcome back, ${currentAccount.username}`; //labelWelcome does not change the text??? FIX THIS
-  } else {
-    //Prevent form from submitting
-    e.preventDefault();
-    alert("Wrong Username or Password.");
-    inputLoginUser.value = "";
-    inputLoginPsw.value = "";
-  }
+/*OKTA LOGIN SCRIPT*/
+const oktaSignIn = new OktaSignIn({
+  baseUrl: "https://dev-30200724.okta.com",
+  clientId: "0oa6a3hqxlrvFP4g55d6",
+  authParams: {
+    issuer: "https://dev-30200724.okta.com/oauth2/default",
+  },
+  redirectUri: "http://127.0.0.1:8080/index.html",
 });
-//*****************************************************************************/
 
-//REGISTER A USER*******************************
-const registerButton = document.querySelector(".btn");
-const inputRegUser = document.querySelector(".input-field-user");
-const inputRegPsw = document.querySelector(".input-field-psw");
+oktaSignIn.authClient.token.getUserInfo().then(
+  function (user) {
+    document.getElementById("messageBox").innerHTML =
+      "Hello, " + user.email + "! You are *still* logged in! :)";
+    document.getElementById("logout").style.display = "block"; //Logout button
+  },
+  function (error) {
+    oktaSignIn
+      .showSignInToGetTokens({
+        el: "#okta-login-container",
+      })
+      .then(function (tokens) {
+        oktaSignIn.authClient.tokenManager.setTokens(tokens);
+        oktaSignIn.remove();
 
-let newActivity = {};
-registerButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  newActivity = {};
-  var inputs = document.querySelectorAll(".reg__form input");
-  for (var i = 0; i < inputs.length; i++) {
-    newActivity[inputs[i].name] = inputs[i].value;
+        const idToken = tokens.idToken;
+        document.getElementById("messageBox").innerHTML =
+          "Hello, " + idToken.claims.email + "! You just logged in! :)";
+        document.getElementById("logout").style.display = "block";
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   }
-  if (inputRegPsw.value.length >= 6 && inputRegUser.value.length >= 3) {
-    accounts.push(newActivity);
-    console.log(accounts);
-    inputRegUser.value = ""; //resetting input boxes to default (empty)
-    inputRegPsw.value = ""; //resetting input boxes to default (empty)
-    alert("User added!");
-  } else {
-    alert(
-      "Your Username or Password doesn't match the critera.\n(min username length: 3) AND (min password length: 6)"
-    );
-    event.preventDefault();
-  }
-});
-console.log(accounts);
-//************************************************************************************************* */
+);
 
-//Force username inputs to be lowercase on registration AND login
-function forceLower(strInput) {
-  strInput.value = strInput.value.toLowerCase();
+function logout() {
+  oktaSignIn.authClient.signOut();
+  location.reload();
 }
-//******************************************************************** */
+/*END OF OKTA LOGIN SCRIPT*/
